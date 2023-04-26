6743c3cf503bc8a62d96f0d1f4c0bf95c8c4ca6b
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-04-26 10:20:59 -0300

    insert new method

---
+++ offset.js
@@ -0,0 +1,37 @@
+function cifrar(offset, mensagem) {
+  let resultado = "";
+  for (let i = 0; i < mensagem.length; i++) {
+    let codLetraAscii = mensagem.charCodeAt(i);
+    if (codLetraAscii >= 65 && codLetraAscii <= 90) {
+      let calculoOffset = ((codLetraAscii - 65 + offset) % 26) + 65;
+      resultado += String.fromCharCode(calculoOffset);
+    } else if (codLetraAscii >= 97 && codLetraAscii <= 122) {
+      let calculoOffset = ((codLetraAscii - 97 + offset) % 26) + 97;
+      resultado += String.fromCharCode(calculoOffset);
+    } else {
+      resultado += String.fromCharCode(codLetraAscii);
+    }
+  }
+  return resultado;
+}
+
+console.log(cifrar(3, "Zumbis"));
+
+function decode(offset, mensagem) {
+  let resultado = "";
+  for (let i = 0; i < mensagem.length; i++) {
+    let codLetraAscii = mensagem.charCodeAt(i);
+    if (codLetraAscii >= 65 && codLetraAscii <= 90) {
+      let calculoOffset = ((codLetraAscii - 90 - offset) % 26) + 90;
+      resultado += String.fromCharCode(calculoOffset);
+    } else if (codLetraAscii >= 97 && codLetraAscii <= 122) {
+      let calculoOffset = ((codLetraAscii - 122 - offset) % 26) + 122;
+      resultado += String.fromCharCode(calculoOffset);
+    } else {
+      resultado += String.fromCharCode(codLetraAscii);
+    }
+  }
+  return resultado;
+}
+
+console.log(decode(3, "Cxpelv"));
---
+++ unicode.js
@@ -0,0 +1,5 @@
+const mensagem = "Hoje";
+
+for (let i = 0; i < mensagem.length; i++) {
+ console.log(mensagem.charCodeAt(i));
+}
